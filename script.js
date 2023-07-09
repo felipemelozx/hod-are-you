d09e90f649b6926b42408f4033e651e1ea889a28
felipemelozx <127111346+felipemelozx@users.noreply.github.com>
2023-07-09 16:41:21 -0300

    function cal old and gener

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ function check() {
     var data = new Date();
     var year = data.getFullYear();
     var fano = document.querySelector('.text');
-    var ress = document.querySelector('.result');
-
-    if (fano.value.length === 0 || fano.value > year) {
+    var res = document.querySelector('.result');
+    
+    if (fano.value.length === 0 || Number(fano.value) > year) {
         alert('[ERRO] Verifique os dados e tente novamente');
     } else {
-        alert('Tudo ok');
+        var fsex = document.getElementsByName('sex')
+        var idade = year - Number(fano.value)
+        var genero = ''
+        if (fsex[0].checked){
+            genero = 'male'
+        } else {
+            genero = 'woman'
+           
+        }
+        res.innerHTML = `we detect a ${genero} age ${idade}`
     }
 }
